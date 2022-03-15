8e357b961413d32b1fe8abc11bccf02e5f85fd91
t.inoue <takayuki.inoue@i-studio.co.jp>
2022-03-16 02:54:56 +0900

    #1 タブ切り替えできるように修正

--- src/assets/js/components/Organisms/Code.ts
+++ src/assets/js/components/Organisms/Code.ts
@@ -4,10 +4,28 @@ import {css} from "@emotion/css";
 class Code extends View.Component {
   constructor(props) {
     super(props);
-    console.log(this)
     this.init(()=>{
 
     })
+  }
+
+  clickJS = (e: Event) => {
+    this.clearActive();
+    const target = e.target as HTMLButtonElement;
+    const tag = target.getAttribute("data-type");
+    this.refs[tag].classList.add("is-active");
+    target.classList.add("is-active");
+  }
+
+  clearActive = () => {
+    const tags = ["js", "html", "css"];
+    for (const tag of tags) {
+      this.refs[tag].classList.remove("is-active");
+    }
+    const buttonArray = this.refs.buttons.querySelectorAll("button");
+    for (const button of buttonArray) {
+      button.classList.remove("is-active");
+    }
   }
 
   style = () => {
@@ -18,6 +36,31 @@ class Code extends View.Component {
       title: css({
         fontSize: "20px",
         marginBottom: "-50px"
+      }),
+      codeBox: css({
+        display: "none",
+        "&.is-active": {
+          display: "block"
+        }
+      }),
+      buttons: css({
+        display: "grid",
+        gridTemplateColumns: "33% 33% 33%",
+        gridTemplateRows: "50px",
+        button: {
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#fff",
+          color: "#000",
+          border: "solid 1px #000",
+          "&.is-active": {
+            backgroundColor: "#000",
+            color: "#fff"
+          }
+        }
       })
     }
   }
